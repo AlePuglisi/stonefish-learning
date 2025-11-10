e8857c6224c3f4db119cbcfcf2beec982fe974e7
Alessandro Puglisi <puglisialessandro27@gmail.com>
2025-11-10 23:37:33 +0100

    chore: remove stonefish_ws/build directory

--- stonefish_ws/build/bluerov2_control/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/ale/tutorials/stonefish-learning/stonefish_ws/install/bluerov2_control'
